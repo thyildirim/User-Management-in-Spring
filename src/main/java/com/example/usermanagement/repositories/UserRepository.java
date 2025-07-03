e9275824d547cceff3aa7444c57cb8a7f9517b34
thyildirim <taha0yldrm@outlook.com>
2025-07-03 22:59:51 +0300

    UserRepository eklendi

--- src/main/java/com/example/usermanagement/repositories/UserRepository.java
+++ src/main/java/com/example/usermanagement/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.example.usermanagement.repositories;
 
 import com.example.usermanagement.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
-    User createUser(User user);
+public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User user);
     User findByUsername(String username);
 }
