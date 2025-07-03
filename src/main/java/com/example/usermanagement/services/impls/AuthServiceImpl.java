09c3d0c3b554c05038bc0d91ad2d81211d881342
thyildirim <taha0yldrm@outlook.com>
2025-07-03 23:09:13 +0300

    AuthServiceImpl düzenlendi

--- src/main/java/com/example/usermanagement/services/impls/AuthServiceImpl.java
+++ src/main/java/com/example/usermanagement/services/impls/AuthServiceImpl.java
@@ -4,18 +4,37 @@ import com.example.usermanagement.dtos.login.LoginRequest;
 import com.example.usermanagement.dtos.login.LoginResponse;
 import com.example.usermanagement.dtos.register.RegisterRequest;
 import com.example.usermanagement.dtos.register.RegisterResponse;
+import com.example.usermanagement.entities.User;
+import com.example.usermanagement.repositories.UserRepository;
 import com.example.usermanagement.services.AuthService;
 
 public class AuthServiceImpl implements AuthService {
 
+    private final UserRepository userRepository;
+
+    public AuthServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public RegisterResponse register(RegisterRequest request) {
+        User newUser = new User(request.username(), request.password());
+        userRepository.save(newUser);
         return new RegisterResponse();
     }
 
     @Override
     public LoginResponse login(LoginRequest request) {
+        User existingUser = userRepository.findByUsername(request.username());
+        if(existingUser == null) {
+            throw new RuntimeException("user not found");
+        }
+
+        boolean isPasswordCorrect = existingUser.getPassword() == request.password();
+        if(!isPasswordCorrect) {
+            throw new RuntimeException("invalid credentials");
+        }
+
         return new LoginResponse();
     }
 }
