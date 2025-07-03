ced7b780923e5e2d085e5e616772a895c5d3ffc5
thyildirim <taha0yldrm@outlook.com>
2025-07-03 22:42:09 +0300

    User tablosu eklendi

---
+++ src/main/java/com/example/usermanagement/entities/User.java
@@ -0,0 +1,20 @@
+package com.example.usermanagement.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+}
