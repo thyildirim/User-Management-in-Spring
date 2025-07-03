77e253ca46de659566fd753f2c24c8170188d145
thyildirim <taha0yldrm@outlook.com>
2025-07-04 00:04:05 +0300

    rename

--- src/main/java/com/example/usermanagement/exceptions/NotFoundExceptionHandler.java
+++ src/main/java/com/example/usermanagement/exceptions/NotFoundExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
-public class UserNotFoundExceptionHandler {
+public class NotFoundExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(NotFoundException ex) {
