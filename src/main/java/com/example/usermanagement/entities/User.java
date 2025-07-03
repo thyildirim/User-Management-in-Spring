85902533546e01b534a8e3a67a2186aa39e16008
thyildirim <taha0yldrm@outlook.com>
2025-07-03 22:46:28 +0300

    validation eklendi

--- src/main/java/com/example/usermanagement/entities/User.java
+++ src/main/java/com/example/usermanagement/entities/User.java
@@ -1,6 +1,8 @@
 package com.example.usermanagement.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,5 +18,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "username should not be blank")
+    @Size(min = 3, max = 10)
     private String username;
+
+    @NotBlank(message = "password should not be blank")
+    @Size(min = 3, max = 10)
+    private String password;
 }
