a53c410173fa615bce8507a71ee61f3383915db7
thyildirim <taha0yldrm@outlook.com>
2025-07-04 00:57:13 +0300

    çalışmayan test ekle

---
+++ src/test/java/com/example/usermanagement/controllers/AuthControllerTest.java
@@ -0,0 +1,59 @@
+package com.example.usermanagement.controllers;
+
+import com.example.usermanagement.dtos.login.LoginRequest;
+import com.example.usermanagement.dtos.register.RegisterRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AuthController.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testRegisterAndLogin() throws Exception {
+        String username = "username";
+        String password = "password";
+
+        // REGISTER
+        RegisterRequest registerRequest = new RegisterRequest(username, password);
+
+        mockMvc.perform(post("/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(registerRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("User registered successfully!"));
+
+        // LOGIN
+        LoginRequest loginRequest = new LoginRequest("username","password");
+
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("jwt-token-example"));
+    }
+
+    @Test
+    public void testOnlyLogin() throws Exception {
+        LoginRequest loginRequest = new LoginRequest("username", "password");
+
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("only-login-token"));
+    }
+}
